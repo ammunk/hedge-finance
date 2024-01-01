62be43eb933729843769539cb874472d53b6b1d8
Andreas Munk <andreas@ammunk.com>
2024-01-01 15:36:19 -0800

    add stuff

---
+++ what-is-t2k/.auctex-auto/business_plan.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "business_plan"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "svg"
+    "biblatex")
+   (LaTeX-add-labels
+    "sec:org940bf5b")
+   (LaTeX-add-bibliographies
+    "~/.dotfiles/bibtex"))
+ :latex)
+
